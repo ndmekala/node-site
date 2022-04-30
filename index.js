1f743f3c58d9dfb753a0b39b206a2d6828e4084b
Nirmal Mekala <ndmekala@icloud.com>
2022-04-29 20:12:16 -0500

    beginning of http server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('yo!')
+const http = require('http')
+
+const port = 3000
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<p>hi</p>')
+})
+
+server.listen(port, () => {
+    console.log('up n running!')
+})
